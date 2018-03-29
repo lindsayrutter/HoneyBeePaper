202814b477b1414a64a13771d2f4a537da6df879
Rutter <lindsayannerutter@gmail.com>
2018-03-29 10:44:41 -0500

    Add all DEGs for VirusHoneyBee group

--- VirusHoneyBee/data/makeData.R
+++ VirusHoneyBee/data/makeData.R
@@ -4,7 +4,8 @@ library("readr")
 d <- read_delim("GSE65659_AntiviralResponseReadCounts.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
 
 data <- as.data.frame(d)
-data <- data[,1:7]
-colnames(data) <- c("ID", "C.1", "C.2", "C.3", "T.1", "T.2", "T.3")
+rownames(data) <- data[,1]
+data <- data[,2:7]
+colnames(data) <- c("C.1", "C.2", "C.3", "T.1", "T.2", "T.3")
 
 saveRDS(data, "data.Rds")
