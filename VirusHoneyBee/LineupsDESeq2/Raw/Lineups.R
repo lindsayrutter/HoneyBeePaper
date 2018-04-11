6c75d9e98ebb10b7dffd1fe0b5ea56aad3d9fe90
Rutter <lindsayannerutter@gmail.com>
2018-04-10 21:33:24 -0500

    Standard scatterplots Gailbraith data

---
+++ VirusHoneyBee/LineupsDESeq2/Raw/Lineups.R
@@ -0,0 +1,15 @@
+library(edgeR)
+library(ggplot2)
+library(GGally)
+library(EDASeq)
+library(utils)
+library(data.table)
+library(bigPint)
+
+thisPath <- getwd()
+
+beeCounts <- readRDS("../../data/data.Rds")
+beeCounts <- cbind(ID=rownames(beeCounts), beeCounts)
+beeCounts$ID <- as.character(beeCounts$ID)
+
+plotPermutationsD(beeCounts, nPerm = 10, topThresh = 50, outDir = getwd())
