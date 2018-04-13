811352e762932d30f52af0bd5b346d8ce8733583
Rutter <lindsayannerutter@gmail.com>
2018-04-13 12:49:57 -0500

    Venn updates

--- Overlaps/Overlaps.R
+++ Overlaps/Overlaps.R
@@ -2,173 +2,169 @@ library(VennDiagram)
 library(readr)
 library(readxl)
 
-#draw.triple.venn(65, 75, 85, 35, 15, 25, 5, c("First", "Second", "Third"))
+#######################################################
+# Read in DEG files
+RDC <- readRDS("../C_R/DESeq2/RDC.Rds")
+RDR <- readRDS("../C_R/DESeq2/RDR.Rds")
+RD_DIET_TOTAL <- readRDS("../C_R/DESeq2/RD_DIET_TOTAL.Rds")
+REC <- readRDS("../C_R/EdgeR/edgeR/REC.Rds")
+RER <- readRDS("../C_R/EdgeR/edgeR/RER.Rds")
+RE_DIET_TOTAL <- readRDS("../C_R/EdgeR/edgeR/RE_DIET_TOTAL.Rds")
+RLC <- readRDS("../C_R/LimmaVoom/RLC.Rds")
+RLR <- readRDS("../C_R/LimmaVoom/RLR.Rds")
+RL_DIET_TOTAL <- readRDS("../C_R/LimmaVoom/RL_DIET_TOTAL.Rds")
+RDV <- readRDS("../N_V/DESeq2/RDV.Rds")
+RDN <- readRDS("../N_V/DESeq2/RDN.Rds")
+RD_VIRUS_TOTAL <- readRDS("../N_V/DESeq2/RD_VIRUS_TOTAL.Rds")
+REV <- readRDS("../N_V/EdgeR/edgeR/REV.Rds")
+REN <- readRDS("../N_V/EdgeR/edgeR/REN.Rds")
+RE_VIRUS_TOTAL <- readRDS("../N_V/EdgeR/edgeR/RE_VIRUS_TOTAL.Rds")
+GDV <- readRDS("../VirusHoneyBee/DESeq2/GDV.Rds")
+GDC <- readRDS("../VirusHoneyBee/DESeq2/GDC.Rds")
+GD_TOTAL <- readRDS("../VirusHoneyBee/DESeq2/GD_TOTAL.Rds")
+GEV <- readRDS("../VirusHoneyBee/EdgeR/edgeR/GEV.Rds")
+GEC <- readRDS("../VirusHoneyBee/EdgeR/edgeR/GEC.Rds")
+GE_TOTAL <- readRDS("../VirusHoneyBee/EdgeR/edgeR/GE_TOTAL.Rds")
+GLV <- readRDS("../VirusHoneyBee/LimmaVoom/GLV.Rds")
+GLC <- readRDS("../VirusHoneyBee/LimmaVoom/GLC.Rds")
+GL_TOTAL <- readRDS("../VirusHoneyBee/LimmaVoom/GL_TOTAL.Rds")
 
 #######################################################
 # Get overlap between all genes between two studies
-dataT = read.delim(file="../N_V/data/AllLaneCount.txt",row.names=1,stringsAsFactors = FALSE)
+# namesG has length 15314; namesR has length 11825
+# Overlap has 11825
+
+dataR = read.delim(file="../N_V/data/AllLaneCount.txt",row.names=1,stringsAsFactors = FALSE)
 dataG = read_delim("../VirusHoneyBee/data/GSE65659_AntiviralResponseReadCounts.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
 dataG <- as.data.frame(dataG)
 
-# namesG has length 15314; namesT has length 11825
-namesT = rownames(dataT)
+namesR = rownames(dataR)
 namesG = unname(sapply(dataG$id, function(x) strsplit(strsplit(x, "[|]")[[1]][3], "[-]")[[1]][1]))
 
-N = intersect(namesG, namesT)
+N = intersect(namesG, namesR)
 N = length(N)
-#######################################################
 
-############# Venn diagrams for Galbraith ############# 
-####################################################### 
+############# Venn diagrams for Galbraith (Total) ############# 
 
-colList = scales::hue_pal()(4)
+int_GD_GE_TOTAL = intersect(GD_TOTAL, GE_TOTAL)
+int_GD_GL_TOTAL = intersect(GD_TOTAL, GL_TOTAL)
+int_GE_GL_TOTAL = intersect(GE_TOTAL, GL_TOTAL)
+int_GD_GE_GL_TOTAL = intersect(intersect(GD_TOTAL, GE_TOTAL), GL_TOTAL)
 
-gDESeq = readRDS("../VirusHoneyBee/DESeq2/Method1/dataMetrics.Rds")
-gDESeq = gDESeq[["C_T"]]
-gD = gDESeq[which(gDESeq$padj <0.05),]$ID
-
-gEdgeR = readRDS("../VirusHoneyBee/EdgeR/edgeR/dataMetrics.Rds")
-gEdgeR = gEdgeR[["C_T"]]
-gE = gEdgeR[which(gEdgeR$FDR <0.05),]$ID
-
-gEdgeRB = readRDS("../VirusHoneyBee/EdgeR/edgeR-btwnLane/dataMetrics.Rds")
-gEdgeRB = gEdgeRB[["C_T"]]
-gEB = gEdgeRB[which(gEdgeRB$FDR <0.05),]$ID
-
-gLimma = readRDS("../VirusHoneyBee/LimmaVoom/dataMetrics.Rds")
-gLimma = gLimma[["C_T"]]
-gL = gLimma[which(gLimma$adj.P.Val <0.05),]$ID
-
-intgD = length(gD)
-intgE = length(gE)
-intgEB = length(gEB)
-intgL = length(gL)
-
-gDE = intersect(gD, gE)
-gDEB = intersect(gD, gEB)
-gDL = intersect(gD, gL)
-gEEB = intersect(gE, gEB)
-gEL = intersect(gE, gL)
-gEBL = intersect(gEB, gL)
-gDEEB = intersect(intersect(gD, gE), gEB)
-gDEL = intersect(intersect(gD, gE), gL)
-gDEBL = intersect(intersect(gD, gEB), gL)
-gEEBL = intersect(intersect(gE, gEB), gL)
-gDEEBL = intersect(intersect(gE, gEB), intersect(gD, gL))
-
-intgDE = length(gDE)
-intgDEB = length(gDEB)
-intgDL = length(gDL)
-intgEEB = length(gEEB)
-intgEL = length(gEL)
-intgEBL = length(gEBL)
-intgDEEB = length(gDEEB)
-intgDEL = length(gDEL)
-intgDEBL = length(gDEBL)
-intgEEBL = length(gEEBL)
-intgDEEBL = length(gDEEBL)
-
-fileName = paste(getwd(), "/Venn_Galbraith.jpg", sep="")
+fileName = paste(getwd(), "/Venn_Galbraith_Total.jpg", sep="")
 jpeg(fileName)
-draw.quad.venn(add.title = "Test", area1=intgD, area2=intgE, area3=intgEB, area4=intgL, n12=intgDE, n13=intgDEB, n14=intgDL, n23=intgEEB, n24=intgEL, n34=intgEBL, n123=intgDEEB, n124=intgDEL, n134=intgDEBL, n234=intgEEBL, n1234=intgDEEBL, c("g-DESeq2", "g-EdgeR", "g-EdgeR-btwn", "g-Limma"), cat.col = colList)
+draw.triple.venn(area1=length(GD_TOTAL), area2=length(GE_TOTAL), area3=length(GL_TOTAL), n12=length(int_GD_GE_TOTAL), n13=length(int_GD_GL_TOTAL), n23=length(int_GE_GL_TOTAL), n123=length(int_GD_GE_GL_TOTAL), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-############### Venn diagrams for Toth ############## 
-##################################################### 
+############# Venn diagrams for Galbraith (Virus) ############# 
 
-tDESeq = readRDS("../N_V/DESeq2/Method1/dataMetrics.Rds")
-tDESeq = tDESeq[["N_V"]]
-tD = tDESeq[which(tDESeq$padj <0.05),]$ID
+int_GDV_GEV = intersect(GDV, GEV)
+int_GDV_GLV = intersect(GDV, GLV)
+int_GEV_GLV = intersect(GEV, GLV)
+int_GDV_GEV_GLV = intersect(intersect(GDV, GEV), GLV)
 
-tEdgeR = readRDS("../N_V/EdgeR/edgeR/dataMetrics.Rds")
-tEdgeR = tEdgeR[["N_V"]]
-tE = tEdgeR[which(tEdgeR$FDR <0.05),]$ID
-
-tEdgeRB = readRDS("../N_V/EdgeR/edgeR-btwnLane/dataMetrics.Rds")
-tEdgeRB = tEdgeRB[["N_V"]]
-tEB = tEdgeRB[which(tEdgeRB$FDR <0.05),]$ID
-
-tLimma = readRDS("../N_V/LimmaVoom/dataMetrics.Rds")
-tLimma = tLimma[["N_V"]]
-tL = tLimma[which(tLimma$adj.P.Val <0.05),]$ID
-
-inttD = length(tD)
-inttE = length(tE)
-inttEB = length(tEB)
-inttL = length(tL)
-
-tDE = intersect(tD, tE)
-tDEB = intersect(tD, tEB)
-tDL = intersect(tD, tL)
-tEEB = intersect(tE, tEB)
-tEL = intersect(tE, tL)
-tEBL = intersect(tEB, tL)
-tDEEB = intersect(intersect(tD, tE), tEB)
-tDEL = intersect(intersect(tD, tE), tL)
-tDEBL = intersect(intersect(tD, tEB), tL)
-tEEBL = intersect(intersect(tE, tEB), tL)
-tDEEBL = intersect(intersect(tE, tEB), intersect(tD, tL))
-
-inttDE = length(tDE)
-inttDEB = length(tDEB)
-inttDL = length(tDL)
-inttEEB = length(tEEB)
-inttEL = length(tEL)
-inttEBL = length(tEBL)
-inttDEEB = length(tDEEB)
-inttDEL = length(tDEL)
-inttDEBL = length(tDEBL)
-inttEEBL = length(tEEBL)
-inttDEEBL = length(tDEEBL)
-
-fileName = paste(getwd(), "/Venn_Toth.jpg", sep="")
+fileName = paste(getwd(), "/Venn_Galbraith_Virus.jpg", sep="")
 jpeg(fileName)
-draw.quad.venn(area1=inttD, area2=inttE, area3=inttEB, area4=inttL, n12=inttDE, n13=inttDEB, n14=inttDL, n23=inttEEB, n24=inttEL, n34=inttEBL, n123=inttDEEB, n124=inttDEL, n134=inttDEBL, n234=inttEEBL, n1234=inttDEEBL, c("t-DESeq2", "t-EdgeR", "t-EdgeR-bwn", "t-Limma"), cat.col = colList)
+draw.triple.venn(area1=length(GDV), area2=length(GEV), area3=length(GLV), n12=length(int_GDV_GEV), n13=length(int_GDV_GLV), n23=length(int_GEV_GLV), n123=length(int_GDV_GEV_GLV), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-fileName = paste(getwd(), "/Venn_Toth_NoLimma.jpg", sep="")
+############# Venn diagrams for Galbraith (Control) ############# 
+
+int_GDC_GEC = intersect(GDC, GEC)
+int_GDC_GLC = intersect(GDC, GLC)
+int_GEC_GLC = intersect(GEC, GLC)
+int_GDC_GEC_GLC = intersect(intersect(GDC, GEC), GLC)
+
+fileName = paste(getwd(), "/Venn_Galbraith_Control.jpg", sep="")
 jpeg(fileName)
-draw.triple.venn(area1=inttD, area2=inttE, area3=inttEB, n12=inttDE, n13=inttDEB, n23=inttEEB, n123=inttDEEB, c("t-DESeq2", "t-EdgeR", "t-EdgeR-bwn"), cat.col = colList[1:3])
+draw.triple.venn(area1=length(GDC), area2=length(GEC), area3=length(GLC), n12=length(int_GDC_GEC), n13=length(int_GDC_GLC), n23=length(int_GEC_GLC), n123=length(int_GDC_GEC_GLC), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-############# Venn diagrams for Galbraith versus Toth per Method ############# 
-##############################################################################
+############### Venn diagrams for Rutter (Diet Total) ############## 
 
-gD2 = sapply(gD, function(x) strsplit(strsplit(x, "[|]")[[1]][3], "[-]")[[1]][1])
-fileName = paste(getwd(), "/Venn_DESeq2.jpg", sep="")
+int_RD_RE_DIET_TOTAL = intersect(RD_DIET_TOTAL, RE_DIET_TOTAL)
+int_RD_RL_DIET_TOTAL = intersect(RD_DIET_TOTAL, RL_DIET_TOTAL)
+int_RE_RL_DIET_TOTAL = intersect(RE_DIET_TOTAL, RL_DIET_TOTAL)
+int_RD_RE_RL_DIET_TOTAL = intersect(intersect(RD_DIET_TOTAL, RE_DIET_TOTAL), RL_DIET_TOTAL)
+
+fileName = paste(getwd(), "/Venn_Rutter_Diet_Total.jpg", sep="")
 jpeg(fileName)
-tgD = intersect(gD2, tD)
-draw.pairwise.venn(area1=intgD, area2=inttD, cross.area = length(tgD), category=c("g-DESeq2", "t-DESeq2"))
+draw.triple.venn(area1=length(RD_DIET_TOTAL), area2=length(RE_DIET_TOTAL), area3=length(RL_DIET_TOTAL), n12=length(int_RD_RE_DIET_TOTAL), n13=length(int_RD_RL_DIET_TOTAL), n23=length(int_RE_RL_DIET_TOTAL), n123=length(int_RD_RE_RL_DIET_TOTAL), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-# Overlap between T and G for DESeq2 is significant at p-value < 2.2e-16
-# 11825 | 16 | 992 | 27
-fisher.test(matrix(c(N, intgD-DcrossArea, inttD-DcrossArea, DcrossArea), nrow=2), alternative="greater")
+############### Venn diagrams for Rutter (Diet Castanea) ############## 
 
-gE2 = sapply(gE, function(x) strsplit(strsplit(x, "[|]")[[1]][3], "[-]")[[1]][1])
-fileName = paste(getwd(), "/Venn_EdgeR.jpg", sep="")
+int_RDC_REC = intersect(RDC, REC)
+int_RDC_RLC = intersect(RDC, RLC)
+int_REC_RLC = intersect(REC, RLC)
+int_RDC_REC_RLC = intersect(intersect(RDC, REC), RLC)
+
+fileName = paste(getwd(), "/Venn_Rutter_Diet_Castanea.jpg", sep="")
 jpeg(fileName)
-tgE = intersect(gE2, tE)
-draw.pairwise.venn(area1=intgE, area2=inttE, cross.area = length(tgE), category=c("g-EdgeR", "t-EdgeR"))
+draw.triple.venn(area1=length(RDC), area2=length(REC), area3=length(RLC), n12=length(int_RDC_REC), n13=length(int_RDC_RLC), n23=length(int_REC_RLC), n123=length(int_RDC_REC_RLC), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-# Overlap between T and G for EdgeR is significant at p-value = 9.501e-10
-# 11825 | 9 | 653 | 11
-fisher.test(matrix(c(N, intgE-EcrossArea, inttE-EcrossArea, EcrossArea), nrow=2), alternative="greater")
+############### Venn diagrams for Rutter (Diet Rockrose) ############## 
 
-gEB2 = sapply(gEB, function(x) strsplit(strsplit(x, "[|]")[[1]][3], "[-]")[[1]][1])
-fileName = paste(getwd(), "/Venn_EdgeR-btwn.jpg", sep="")
+int_RDR_RER = intersect(RDR, RER)
+int_RDR_RLR = intersect(RDR, RLR)
+int_RER_RLR = intersect(RER, RLR)
+int_RDR_RER_RLR = intersect(intersect(RDR, RER), RLR)
+
+fileName = paste(getwd(), "/Venn_Rutter_Diet_Rockrose.jpg", sep="")
 jpeg(fileName)
-tgEB = intersect(gEB2, tEB)
-draw.pairwise.venn(area1=intgEB, area2=inttEB, cross.area = length(tgEB), category=c("g-EdgeR-btwn", "t-EdgeR-btwn"))
+draw.triple.venn(area1=length(RDR), area2=length(RER), area3=length(RLR), n12=length(int_RDR_RER), n13=length(int_RDR_RLR), n23=length(int_RER_RLR), n123=length(int_RDR_RER_RLR), c("DESeq2", "EdgeR", "Limma"))
 invisible(dev.off())
 
-# Overlap between T and G for EdgeR-btwn is significant at p-value = 4.32e-05
-# 11825 | 7 | 3317 | 13
-fisher.test(matrix(c(N, intgEB-EBcrossArea, inttEB-EBcrossArea, EBcrossArea), nrow=2), alternative="greater")
+############### Rutter Diet (overlaps between up and down) ############## 
+
+# No cases where upregulated in one pipeline (DESeq, EdgeR, Limma) and downregulated in another
+length(intersect(RDC, RER))
+length(intersect(RDC, RLR))
+length(intersect(REC, RDR))
+length(intersect(REC, RLR))
+length(intersect(RLC, RDR))
+length(intersect(RLC, RER))
+
+
+
+############### Venn diagrams for Rutter (Virus Total) ############## 
+
+int_RD_RE_VIRUS_TOTAL = intersect(RD_VIRUS_TOTAL, RE_VIRUS_TOTAL)
+
+fileName = paste(getwd(), "/Venn_Rutter_Virus_Total.jpg", sep="")
+jpeg(fileName)
+draw.pairwise.venn(area1=length(RD_VIRUS_TOTAL), area2=length(RE_VIRUS_TOTAL), cross.area=length(int_RD_RE_VIRUS_TOTAL), c("DESeq2", "EdgeR"))
+invisible(dev.off())
+
+############### Venn diagrams for Rutter (Virus Infected) ############## 
+
+int_RDV_REV = intersect(RDV, REV)
+
+fileName = paste(getwd(), "/Venn_Rutter_Virus_Infected.jpg", sep="")
+jpeg(fileName)
+draw.pairwise.venn(area1=length(RDV), area2=length(REV), cross.area=length(int_RDV_REV), c("DESeq2", "EdgeR"))
+invisible(dev.off())
+
+############### Venn diagrams for Rutter (Virus Healthy) ############## 
+
+int_RDN_REN = intersect(RDN, REN)
+
+fileName = paste(getwd(), "/Venn_Rutter_Virus_Healthy.jpg", sep="")
+jpeg(fileName)
+draw.pairwise.venn(area1=length(RDN), area2=length(REN), cross.area=length(int_RDN_REN), c("DESeq2", "EdgeR"))
+invisible(dev.off())
+
+############### Rutter Virus (overlaps between up and down) ############## 
+
+# No cases where upregulated in one pipeline (DESeq, EdgeR, Limma) and downregulated in another
+length(intersect(RDV, REN))
+length(intersect(REV, RDN))
+
+
+
 
 ############### Venn diagrams for Supplemental Galbraith versus Toth per Method ############## 
-##############################################################################################
 
 # Keep 753 DEGs from Galbraith paper
 gDSupp = read_excel("../VirusHoneyBee/GalbraithSuppFile.xlsx")
@@ -186,10 +182,29 @@ colnames(keepDEGs) <- keepColNms
 # Extract 753 IDs from Galbraith paper
 gDSupp = keepDEGs$id
 gDSupp = unname(sapply(gDSupp, function(x) strsplit(strsplit(x, "[|]")[[1]][3], "[-]")[[1]][1]))
+gtDSupp = intersect(gDSupp, GD_TOTAL)
 
-gtDSupp = intersect(gDSupp, gD2)
-draw.pairwise.venn(area1=length(gD), area2=length(gDSupp), cross.area = length(gtDSupp), category=c("Galbraith Us", "Galbraith Supp"))
+fileName = paste(getwd(), "/Venn_Galbraith_GalbraithSupp_Virus.jpg", sep="")
+jpeg(fileName)
+draw.pairwise.venn(area1=length(GD_TOTAL), area2=length(gDSupp), cross.area = length(gtDSupp), category=c("Galbraith Us", "Galbraith Supp"))
 invisible(dev.off())
+
+
+
+
+
+
+############### Venn diagrams for DESeq (Total) Rutter vs. Galbraith ##############
+
+intD = intersect(RD_VIRUS_TOTAL, GD_TOTAL)
+
+fileName = paste(getwd(), "/Venn_GR_DESeq.jpg", sep="")
+jpeg(fileName)
+draw.pairwise.venn(area1=length(GD_TOTAL), area2=length(RD_VIRUS_TOTAL), cross.area = length(intD), category=c("Galbraith Us", "Galbraith Supp"))
+invisible(dev.off())
+
+
+
 
 ###################### Save GO Terms ##################### 
 ##########################################################
