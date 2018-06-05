6fa1dc0b9b7cf8c93aa495d2a3d390f8e1918d4e
lrutter <lindsayannerutter@gmail.com>
2018-06-04 22:43:20 -0700

    Virus, tolerance, resistance R^2 boxplots versus IAPV titers

--- Overlaps/ResistanceTolerance.R
+++ Overlaps/ResistanceTolerance.R
@@ -22,15 +22,55 @@ rsq <- function (x, y) cor(x, y) ^ 2
 Variables <- read_csv("~/HoneyBeePaper/Variables.csv")
 varIAPV <- as.data.frame(Variables[,7])[,1]
 
-RSQ_virus_4 = list()
+boxVirus <- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(boxVirus) <- c("Group", "R2")
 for (i in 1:4){
   temp <- readRDS(paste0("../N_V/DESeq2/ClusterStandard/Sig_4_", i, ".Rds"))
   temp2 <- data[which(rownames(data) %in% temp),]
-  
   temp3 = c()
   for (j in 1:nrow(temp2)){
     temp3[j] <- rsq(as.numeric(temp2[j,]), varIAPV)
   }
-  RSQ_virus_4[[i]] <- temp3
+  boxVirus <- rbind(boxVirus, data.frame(Group = rep(paste0("virus",i), length(temp3)), R2 = temp3))
 }
 
+boxTolerance <- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(boxTolerance) <- c("Group", "R2")
+for (i in 1:4){
+  temp <- readRDS(paste0("../ResistanceTolerance/Clustering_Tolerance/Sig_4_", i, ".Rds"))
+  temp2 <- data[which(rownames(data) %in% temp),]
+  temp3 = c()
+  for (j in 1:nrow(temp2)){
+    temp3[j] <- rsq(as.numeric(temp2[j,]), varIAPV)
+  }
+  boxTolerance <- rbind(boxTolerance, data.frame(Group = rep(paste0("tolerance",i), length(temp3)), R2 = temp3))
+}
+
+boxResistance <- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(boxResistance) <- c("Group", "R2")
+for (i in 1:4){
+  temp <- readRDS(paste0("../ResistanceTolerance/Clustering_Resistance/Sig_4_", i, ".Rds"))
+  temp2 <- data[which(rownames(data) %in% temp),]
+  temp3 = c()
+  for (j in 1:nrow(temp2)){
+    temp3[j] <- rsq(as.numeric(temp2[j,]), varIAPV)
+  }
+  boxResistance <- rbind(boxResistance, data.frame(Group = rep(paste0("resistance",i), length(temp3)), R2 = temp3))
+}
+
+boxData <- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(boxData) <- c("Group", "R2")
+for (i in 1:nrow(data)){
+  temp3[i] <- rsq(as.numeric(data[i,]), varIAPV)
+}
+temp3 <- temp3[-which(is.na(temp3))]
+boxData <- rbind(boxData, data.frame(Group = rep(paste0("data"), length(temp3)), R2 = temp3))
+
+plotVirus = rbind(boxVirus, boxData)
+ggplot(plotVirus, aes(x=Group, y=R2)) + geom_boxplot(fill="palegreen2")
+
+plotTolerance = rbind(boxTolerance, boxData)
+ggplot(plotTolerance, aes(x=Group, y=R2)) + geom_boxplot(fill="palegreen2")
+
+plotResistance = rbind(boxResistance, boxData)
+ggplot(plotResistance, aes(x=Group, y=R2)) + geom_boxplot(fill="palegreen2")
