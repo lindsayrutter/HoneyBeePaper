a1d4180efb15687a091e041a2ca7808ba645b74e
lrutter <lindsayannerutter@gmail.com>
2018-06-05 23:01:35 -0700

    Raw, logged, Between-Lane-Normalization distribution of R^2 values for mortality rates

--- Overlaps/ResistanceTolerance.R
+++ Overlaps/ResistanceTolerance.R
@@ -7,9 +7,39 @@ library(readr)
 #data <- as.data.frame(readRDS("../N_V/data/data.Rds"))
 data <- as.data.frame(readRDS("../NC_NR_VC_VR/data/data.Rds"))
 
+
+# Next lines to standardize
+RowSD = function(x) {
+  sqrt(rowSums((x - rowMeans(x))^2)/(dim(x)[2] - 1))
+}
+
+data_Rownames <- data$ID
+data = data[,-1]
+rownames(data) <- data_Rownames
 # Normalize for sequencing depth and other distributional differences between lanes
+data <- betweenLaneNormalization(as.matrix(data), which="full", round=FALSE)
+data = as.data.frame(data)
+# Add mean and standard deviation for each row/gene
+data = data %>% mutate(mean= rowMeans(as.matrix(.[nms])), stdev=rowSds(as.matrix(.[nms])))
+data$mean <- as.numeric(data$mean)
+rownames(data)=data_Rownames
+data$ID <- data_Rownames
+
+datas <- t(apply(as.matrix(data[,1:nColumns]), 1, scale))
+datas <- as.data.frame(datas)
+colnames(datas) <- colnames(data[,1:nColumns])
+datas$ID <- rownames(datas)
+# Indices of the NAN rows. They had stdev=0 in the filt data
+nID <- which(is.nan(datas$N.1))
+# Set these filtered values that have all same values for samples to 0
+datas[nID,1:nColumns] <- 0
+
+# Next two lines for BLN
 # data <- betweenLaneNormalization(as.matrix(data), which="full", round=FALSE)
 # data = as.data.frame(data)
+
+# Next lines for logging
+# data <- log(data+1)
 
 # Another approach?
 # data <- as.matrix(data)
